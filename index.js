3dbcb8eed782e76ad67d11a2fc04ca2cce5827ac
Victor Leman <victorleman1@gmail.com>
2023-09-12 13:02:28 +0300

    commit

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const gameboard = document.getElementById("gameboard");
 const restartBtn = document.getElementById("restart-btn");
 const trophies1 = document.querySelector(".trophies-1");
 const trophies2 = document.querySelector(".trophies-2");
+const icon1 = document.getElementById("player-1-icon");
+const icon2 = document.getElementById("player-2-icon");
+const roundWinnerText = document.getElementById("round-winner");
+const sound1 = new Audio("./assets/sounds/man-sound.mp3");
+const sound2 = new Audio("./assets/sounds/woman-sound.mp3");
+const soundDraw = new Audio("./assets/sounds/draw-sound.mp3");
 const playerFactory = (name, current, marker, score, trophies, moves) => {
   const isWinner = () => alert(`${name} won a trophy!`);
   return {
@@ -19,8 +25,8 @@ const playerFactory = (name, current, marker, score, trophies, moves) => {
   };
 };
 
-const player1 = playerFactory("player 1", true, "x", 0, 0, []);
-const player2 = playerFactory("player 2", false, "o", 0, 0, []);
+const player1 = playerFactory("player 1", true, "🧙🏼‍♂️", 0, 0, []);
+const player2 = playerFactory("player 2", false, "🧙🏼‍♀️", 0, 0, []);
 const winningPatterns = [
   [1, 2, 3],
   [2, 5, 8],
@@ -34,6 +40,7 @@ const winningPatterns = [
 let countMoves = 0;
 let draw = 0;
 let roundOver = false;
+const playSound = () => {};
 const resetGame = () => {
   player1.moves = [];
   player2.moves = [];
@@ -44,15 +51,23 @@ const resetGame = () => {
     cell.innerHTML = "";
   });
 };
+const turnIndicator = () => {
+  player1.current
+    ? ((icon1.style.animation = "up-and-down 1s infinite alternate"),
+      (icon2.style.animation = ""))
+    : ((icon2.style.animation = "up-and-down 1s infinite alternate"),
+      (icon1.style.animation = ""));
+};
 const displayScores = () => {
   score1.innerHTML = player1.score;
   score2.innerHTML = player2.score;
 };
-const trophy = "🏆";
+const trophy = "🍄";
 const displayTrophies = () => {
   trophies1.innerHTML = trophy.repeat(player1.trophies);
   trophies2.innerHTML = trophy.repeat(player2.trophies);
 };
+
 const isSubset = (array1, array2) =>
   array2.every((element) => array1.includes(element));
 
@@ -63,6 +78,7 @@ const endGame = () => {
   displayScores();
   draw = 0;
   scoreDraw.innerHTML = draw;
+  turnIndicator();
 };
 gameboard.addEventListener("click", (e) => {
   let cell = e.target;
@@ -76,6 +92,7 @@ gameboard.addEventListener("click", (e) => {
     currentPlayer.moves.push(parseInt(cell.id));
     player1.current = !player1.current;
     player2.current = !player2.current;
+    turnIndicator();
     for (let i = 0; i < winningPatterns.length; i++) {
       if (isSubset(currentPlayer.moves, winningPatterns[i])) {
         roundOver = true;
@@ -83,14 +100,34 @@ gameboard.addEventListener("click", (e) => {
     }
     if (roundOver) {
       currentPlayer.score += 1;
-      resetGame();
+      player2.current ? sound1.play() : sound2.play();
+      roundWinnerText.style.display = "block";
+      roundWinnerText.textContent = `${currentPlayer.marker} won this round !`;
+      gameboard.style.pointerEvents = "none";
+      setTimeout(() => {
+        roundWinnerText.style.display = "none";
+        gameboard.style.pointerEvents = "";
+        roundWinnerText.textContent = "";
+        resetGame();
+        turnIndicator();
+      }, "4000");
+      countMoves = 0;
       displayScores();
       roundOver = false;
     }
     if (countMoves === 9) {
-      resetGame();
       draw += 1;
+
       scoreDraw.innerHTML = draw;
+      roundWinnerText.style.display = "block";
+      roundWinnerText.textContent = `🌀 That's a draw !`;
+      gameboard.style.pointerEvents = "none";
+      setTimeout(() => {
+        roundWinnerText.style.display = "none";
+        gameboard.style.pointerEvents = "";
+        roundWinnerText.textContent = "";
+        resetGame();
+      }, "4000");
     }
     if (currentPlayer.score === 3) {
       currentPlayer.isWinner();
@@ -106,3 +143,4 @@ restartBtn.addEventListener("click", () => {
   }
 });
 displayTrophies();
+turnIndicator();
