77991b91bafc431498a469bae40199d83be7f22c
Victor Leman <victorleman1@gmail.com>
2023-09-13 13:53:22 +0300

    commit

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
+const game = document.querySelector(".game");
 const cells = document.querySelectorAll(".cell");
-const score1 = document.getElementById("score-1");
-const score2 = document.getElementById("score-2");
-const scoreDraw = document.getElementById("draw-score");
-const gameboard = document.getElementById("gameboard");
-const restartBtn = document.getElementById("restart-btn");
 const trophies1 = document.querySelector(".trophies-1");
 const trophies2 = document.querySelector(".trophies-2");
 const icon1 = document.getElementById("player-1-icon");
 const icon2 = document.getElementById("player-2-icon");
+const score1 = document.getElementById("score-1");
+const scoreDraw = document.getElementById("draw-score");
+const score2 = document.getElementById("score-2");
+const gameboard = document.getElementById("gameboard");
 const roundWinnerText = document.getElementById("round-winner");
 const sound1 = new Audio("./assets/sounds/man-sound.mp3");
 const sound2 = new Audio("./assets/sounds/woman-sound.mp3");
-const soundDraw = new Audio("./assets/sounds/draw-sound.mp3");
+const restartBtn = document.getElementById("restart-btn");
+const modal = document.querySelector(".modal");
+const playerIaBtn = document.getElementById("player-ai-btn");
+const playerPlayerBtn = document.getElementById("player-player-btn");
+
 const playerFactory = (name, current, marker, score, trophies, moves) => {
   return {
     name,
@@ -22,9 +26,9 @@ const playerFactory = (name, current, marker, score, trophies, moves) => {
     moves,
   };
 };
-
 const player1 = playerFactory("player 1", true, "🧙🏼‍♂️", 0, 0, []);
 const player2 = playerFactory("player 2", false, "🧙🏼‍♀️", 0, 0, []);
+//variables
 const winningPatterns = [
   [1, 2, 3],
   [2, 5, 8],
@@ -36,26 +40,48 @@ const winningPatterns = [
   [3, 5, 7],
 ];
 let moves = [];
+let possibleMoves = [];
 let countMoves = 0;
 let draw = 0;
 let roundOver = false;
-let possibleMoves = [];
-const playSound = () => {
-  player2.current ? sound1.play() : sound2.play();
-};
-const resetGame = () => {
-  roundOver = false;
-  player1.moves = [];
-  player2.moves = [];
-  countMoves = 0;
-  possibleMoves = [];
-  moves = [];
-  player1.current = true;
-  player2.current = false;
-  cells.forEach((cell) => {
-    cell.innerHTML = "";
-  });
-};
+let aiPlays = false;
+const trophy = "🍄";
+//EVENTS
+playerIaBtn.addEventListener("click", () => {
+  aiPlays = true;
+  modal.classList.add("hidden");
+  game.classList.remove("hidden");
+});
+playerPlayerBtn.addEventListener("click", () => {
+  modal.classList.add("hidden");
+  game.classList.remove("hidden");
+});
+gameboard.addEventListener("click", (e) => {
+  let cell = e.target;
+  const currentPlayer = player1.current ? player1 : player2;
+  if (cell.textContent == "") {
+    addMove(currentPlayer, cell);
+
+    if (roundOver && currentPlayer.score === 3) {
+      return playerWonGame(currentPlayer);
+    } else if (roundOver && currentPlayer.score < 3) {
+      return playerWonRound(currentPlayer);
+    }
+    if (countMoves === 9) {
+      return drawRound();
+    } else if (aiPlays) return ai();
+  }
+});
+restartBtn.addEventListener("click", () => {
+  if (
+    confirm(
+      "you're about to restart the game, only the trophies will be saved. Do you confirm?"
+    ) == true
+  ) {
+    endGame();
+  }
+});
+//FUNCTIONS
 const ai = () => {
   let allMoves = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   for (let i = 0; i < moves.length; i++) {
@@ -85,24 +111,12 @@ const displayScores = () => {
   score1.innerHTML = player1.score;
   score2.innerHTML = player2.score;
 };
-const trophy = "🍄";
 const displayTrophies = () => {
   trophies1.innerHTML = trophy.repeat(player1.trophies);
   trophies2.innerHTML = trophy.repeat(player2.trophies);
 };
-
 const isSubset = (array1, array2) =>
   array2.every((element) => array1.includes(element));
-
-const endGame = () => {
-  resetGame();
-  turnIndicator();
-  player1.score = 0;
-  player2.score = 0;
-  displayScores();
-  draw = 0;
-  scoreDraw.innerHTML = draw;
-};
 const addMove = (player, cell) => {
   countMoves += 1;
   moves.push(parseInt(cell.id));
@@ -116,30 +130,37 @@ const addMove = (player, cell) => {
   turnIndicator();
   for (let i = 0; i < winningPatterns.length; i++) {
     if (isSubset(player.moves, winningPatterns[i])) {
-      roundOver = true;
-      player.score += 1;
-      displayScores();
+      return (roundOver = true), (player.score += 1), displayScores();
     }
   }
   return roundOver;
 };
+const resetGame = () => {
+  roundOver = false;
+  player1.moves = [];
+  player2.moves = [];
+  countMoves = 0;
+  possibleMoves = [];
+  moves = [];
+  player1.current = true;
+  player2.current = false;
+  cells.forEach((cell) => {
+    cell.innerHTML = "";
+  });
+};
+const endGame = () => {
+  resetGame();
+  turnIndicator();
+  player1.score = 0;
+  player2.score = 0;
+  displayScores();
+  draw = 0;
+  scoreDraw.innerHTML = draw;
+};
+const playSound = () => {
+  player2.current ? sound1.play() : sound2.play();
+};
 
-gameboard.addEventListener("click", (e) => {
-  let cell = e.target;
-  const currentPlayer = player1.current ? player1 : player2;
-  if (cell.textContent == "") {
-    addMove(currentPlayer, cell);
-
-    if (roundOver && currentPlayer.score === 3) {
-      return playerWonGame(currentPlayer);
-    } else if (roundOver && currentPlayer.score < 3) {
-      return playerWonRound(currentPlayer);
-    }
-    if (countMoves === 9) {
-      return drawRound();
-    } else return ai();
-  }
-});
 const drawRound = () => {
   draw += 1;
   scoreDraw.innerHTML = draw;
@@ -186,14 +207,4 @@ const playerWonGame = (player) => {
     turnIndicator();
   }, "4000");
 };
-restartBtn.addEventListener("click", () => {
-  if (
-    confirm(
-      "you're about to restart the game, only the trophies will be saved. Do you confirm?"
-    ) == true
-  ) {
-    endGame();
-  }
-});
-displayTrophies();
 turnIndicator();
